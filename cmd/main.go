7ff397959d4f55742b5c2f467dcedc27dbd7634c
Mykola Titov <kolatitov132@gmail.com>
2025-12-25 15:09:05 +0200

    Add registration realization

--- cmd/main.go
+++ cmd/main.go
@@ -9,81 +9,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// type RegisterRequest struct {
-// 	Username string `json:"username" binding:"required,min=3,max=32,alphanum"`
-// 	Email    string `json:"email"    binding:"required,email,max=254"`
-// 	Password string `json:"password" binding:"required,min=8,max=72"`
-// }
-
-// var ErrUserConflict = errors.New("user already exists")
-
-// func isUniqueViolation(err error) bool {
-// 	if pqErr, ok := err.(*pq.Error); ok {
-// 		return pqErr.Code == "23505"
-// 	}
-// 	return false
-// }
-
-// func createUser(username, email, hash string) error {
-// 	query := `
-// 		INSERT INTO users (user_name, user_email, password_hash)
-// 		VALUES ($1, $2, $3)
-// 	`
-
-// 	_, err := db.Exec(query, username, email, hash)
-// 	if err != nil {
-// 		fmt.Println("DB ERROR:", err) // <--- додано для дебагу
-// 		if isUniqueViolation(err) {
-// 			return ErrUserConflict
-// 		}
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func Register(c *gin.Context) {
-// 	var req RegisterRequest
-
-// 	if err := c.ShouldBindJSON(&req); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid RegisterRequest input"})
-// 		return
-// 	}
-
-// 	req.Username = strings.ToLower(req.Username)
-// 	req.Email = strings.ToLower(req.Email)
-
-// 	hash, err := bcrypt.GenerateFromPassword(
-// 		[]byte(req.Password),
-// 		bcrypt.DefaultCost,
-// 	)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"error": "internal crypto error",
-// 		})
-// 		return
-// 	}
-
-// 	err = createUser(req.Username, req.Email, string(hash))
-// 	if err != nil {
-// 		if errors.Is(err, ErrUserConflict) {
-// 			c.JSON(http.StatusConflict, gin.H{
-// 				"error": "registration failed",
-// 			})
-// 			return
-// 		}
-
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"error": "internal create user error",
-// 		})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusCreated, gin.H{
-// 		"message": "registration successful",
-// 	})
-// }
-
 func main() {
 	var path = "/Users/nermline/Data/Programing projects/VPN_API_Golang/config.yaml"
 	cfg, err := pkg.LoadConfig(path)
@@ -101,7 +26,7 @@ func main() {
 
 	router := gin.Default()
 	router.POST("/api/v1/auth/register", pkg.RegisterUser(db))
-	router.POST("/api/v1/auth/login", pkg.LoginUser(db))
+	// router.POST("/api/v1/auth/login", pkg.LoginUser(db))
 	router.GET("/api/v1/users/availability", pkg.CheckUsernameAvailability(db))
 	router.GET("/api/v1/email/availability", pkg.CheckEmailAvailability(db))
 	router.Run()
--- pkg/registration.go
+++ pkg/registration.go
@@ -1,11 +1,13 @@
 package pkg
 
 import (
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterRequest struct {
@@ -14,11 +16,37 @@ type RegisterRequest struct {
 	Password string `json:"password" binding:"required,min=8,max=72"`
 }
 
+func GenerateHash(password string) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return hash, nil
+}
+
+func CreateUserInDB(db *sqlx.DB, username string, email string, hash string) error {
+	query := `
+		INSERT INTO users (user_name, user_email, password_hash)
+		VALUES ($1, $2, $3)
+	`
+
+	_, err := db.Exec(query, username, email, hash)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func RegisterUser(db *sqlx.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var req RegisterRequest
 
 		if err := c.ShouldBindJSON(&req); err != nil {
+			log.Println(err)
 			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid data"})
 			return
 		}
@@ -26,22 +54,77 @@ func RegisterUser(db *sqlx.DB) gin.HandlerFunc {
 		req.Username = strings.ToLower(req.Username)
 		req.Email = strings.ToLower(req.Email)
 
+		usernameAvailable, err := CheckDBForUsernameAvailability(db, req.Username)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "internal error"})
+			return
+		}
+
+		emailAvailable, err := CheckDBForEmailAvailability(db, req.Email)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "internal error"})
+			return
+		}
+
+		if !usernameAvailable {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "username already in use"})
+			return
+		}
+
+		if !emailAvailable {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "email already in use"})
+			return
+		}
+
+		hash, err := GenerateHash(req.Password)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "internal error"})
+			return
+		}
+
+		err = CreateUserInDB(db, req.Username, req.Email, string(hash))
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "internal error"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"message": "registration successful",
+		})
 	}
 }
 
-func CheckAvailability(db *sqlx.DB, table string, variable string, value string) {
-	// query := `
-	// 		SELECT EXISTS (
-	// 			SELECT 1 FROM $1 WHERE $2 = $3
-	// 		)
-	// `
+func CheckDBForUsernameAvailability(db *sqlx.DB, value string) (bool, error) {
+	query := `
+			SELECT EXISTS (
+				SELECT 1 FROM users WHERE user_name = $1
+			)
+	`
+	var exists bool
+	if err := db.Get(&exists, query, value); err != nil {
+		log.Println(err)
+		return false, err
+	}
+	return !exists, nil
 
-	// if err := db.Get(&exists); err != nil {
-	// 	c.JSON(http.StatusInternalServerError, gin.H{
-	// 		"error": "database error",
-	// 	})
-	// 	return
-	// }
+}
+
+func CheckDBForEmailAvailability(db *sqlx.DB, value string) (bool, error) {
+	query := `
+			SELECT EXISTS (
+				SELECT 1 FROM users WHERE user_email = $1
+			)
+	`
+	var exists bool
+	if err := db.Get(&exists, query, value); err != nil {
+		log.Println(err)
+		return false, err
+	}
+	return !exists, nil
 
 }
 
@@ -55,7 +138,15 @@ func CheckUsernameAvailability(db *sqlx.DB) gin.HandlerFunc {
 			return
 		}
 
-		CheckAvailability(db, "users", "username", username)
+		if available, err := CheckDBForUsernameAvailability(db, username); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Internal error",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"available": available,
+			})
+		}
 	}
 }
 
@@ -69,8 +160,15 @@ func CheckEmailAvailability(db *sqlx.DB) gin.HandlerFunc {
 			return
 		}
 
-		CheckAvailability(db, "users", "email", email)
-
+		if available, err := CheckDBForEmailAvailability(db, email); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Internal error",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"available": available,
+			})
+		}
 	}
 }
 
