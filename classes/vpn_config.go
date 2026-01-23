40f8e3e7184370efe8d1442ed149e2f2281804f3
Mykola Titov <kolatitov132@gmail.com>
2026-01-23 17:24:23 +0200

    Add vpn_config class

---
+++ classes/vpn_config.go
@@ -0,0 +1,11 @@
+package classes
+
+import "time"
+
+type VPNConfig struct {
+	ID              int       `db:"id"`
+	SessionID       int       `db:"session_id"`
+	InternalIP      string    `db:"internal_ip"`
+	ClientPublicKey string    `db:"client_public_key"`
+	CreatedAt       time.Time `db:"created_at"`
+}
